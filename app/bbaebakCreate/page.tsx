010e7d7b28e5fe4c19ec0c19caf4920c5218d552
hanseulhee <3021062@gmail.com>
2024-12-07 17:36:57 +0900

    [#3] feat: apply input components

--- app/bbaebakCreate/page.tsx
+++ app/bbaebakCreate/page.tsx
@@ -1,10 +1,69 @@
-import SendCertificateBtn from 'app/bbaebakCreate/components/button/SendCertificateBtn';
+'use client';
+import { useValidation } from '@hooks/useValidation';
+import DescriptionInput from 'app/bbaebakCreate/components/input/DescriptionInput';
+import MateInput from 'app/bbaebakCreate/components/input/mate/MateInput';
+import NameInput from 'app/bbaebakCreate/components/input/NameInput';
+import { ERROR_MATE_LENGTH, MATE_MAX_LENGTH } from 'app/constants/validation';
+import {
+  validateDescription,
+  validateMateNameExist,
+  validateName,
+} from 'app/utils/validation';
+import { useState } from 'react';
 
-//* 약속 생성 page
-export default function BbaebakCreate() {
+function BbaebakCreate() {
+  const {
+    value: name,
+    setValue: setName,
+    error: nameError,
+    handleBlur: handleNameBlur,
+  } = useValidation('');
+  const {
+    value: description,
+    setValue: setDescription,
+    error: descriptionError,
+    handleBlur: handleDescriptionBlur,
+  } = useValidation('');
+
+  const [mateNames, setMateNames] = useState<string[]>([]);
+  const [mateCountError, setMateCountError] = useState('');
+
+  const handleMateNameChange = (mateName: string) => {
+    const mateError = validateMateNameExist(mateName, mateNames);
+    setMateCountError(mateError || '');
+    if (!mateError) {
+      setMateNames(prevNames => [...prevNames, mateName]);
+    }
+    if (mateNames.length >= MATE_MAX_LENGTH) {
+      setMateCountError(ERROR_MATE_LENGTH);
+      return;
+    }
+  };
+
+  const isFormValid = !nameError && !descriptionError && !mateCountError;
+
   return (
     <div>
-      <SendCertificateBtn />
+      <NameInput
+        value={name}
+        onChange={e => setName(e.target.value)}
+        error={nameError}
+        onBlur={() => handleNameBlur(validateName)}
+      />
+      <DescriptionInput
+        value={description}
+        onChange={e => setDescription(e.target.value)}
+        error={descriptionError}
+        onBlur={() => handleDescriptionBlur(validateDescription)}
+      />
+      <MateInput
+        mateNames={mateNames}
+        onMateChange={handleMateNameChange}
+        error={mateCountError}
+      />
+      <button disabled={!isFormValid}>제출</button>
     </div>
   );
 }
+
+export default BbaebakCreate;
--- app/constants/validation.ts
+++ app/constants/validation.ts
@@ -5,7 +5,8 @@ export const NAME_STANDARD = /^[A-Za-z0-9ㄱ-ㅎ가-힣]+$/;
 export const ERROR_NAME_VALIDATION = '이름에 특수문자를 포함할 수 없습니다.';
 export const ERROR_NAME_LENGTH = `이름은 ${NAME_MIN_LENGTH}자 이상, ${NAME_MAX_LENGTH}자 이하여야 합니다.`;
 
+export const MATE_MAX_LENGTH = 6;
 export const ERROR_MATE_NAME_EXIST = '이미 등록된 빼박 메이트입니다.';
+export const ERROR_MATE_LENGTH = `빼박 메이트는 최대 ${MATE_MAX_LENGTH}명 지정 가능합니다.`;
 
-export const ERROR_DATE_EMPTY = '빼박 날짜를 선택해 주세요.';
 export const ERROR_DESCRIPTION_EMPTY = '빼박 내용을 입력해 주세요.';
