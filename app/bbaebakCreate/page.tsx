dbfcecf0dfe91b024435b3be60e8d15b98a377b8
hanseulhee <3021062@gmail.com>
2024-12-07 14:30:12 +0900

    [#1] feat: apply sendCertificateBtn component

---
+++ app/bbaebakCreate/page.tsx
@@ -0,0 +1,10 @@
+import SendCertificateBtn from 'app/bbaebakCreate/components/button/SendCertificateBtn';
+
+//* 약속 생성 page
+export default function BbaebakCreate() {
+  return (
+    <div>
+      <SendCertificateBtn />
+    </div>
+  );
+}
