2b9e9920bccaa0c6e84b6de432e5cb531eccaf36
kimsuns <kimksy27@gmail.com>
2024-12-07 21:03:49 +0900

    [#14] feat: image download features

--- app/confirmation/components/button/SaveImageButton.tsx
+++ app/confirmation/components/button/SaveImageButton.tsx
@@ -1,10 +1,22 @@
 'use client';
-export default function SaveImageButton() {
-  const canvas = document.querySelector('signDocument');
 
-  const handleClick = () => {
-    console.log('캔바스다', canvas);
+import html2canvas from 'html2canvas';
+
+export default function SaveImageButton() {
+  const handleSaveImage = async () => {
+    // 빼박 증명서 문서 영역
+    const signDocument = document.getElementById('signDocument');
+
+    if (!signDocument) return;
+
+    const canvas = await html2canvas(signDocument); // 캔버스로 변환
+    const link = document.createElement('a');
+    link.href = canvas.toDataURL('image/png');
+    link.download = 'bbaebak-certificate.png';
+    link.click();
+
+    console.log('이미 저장히기', signDocument);
   };
 
-  return <button onClick={handleClick}>갤러리에 저장하기</button>;
+  return <button onClick={handleSaveImage}>갤러리에 저장하기</button>;
 }
