9f6644b2aed65c9eb2aac89922916fbd3234ae13
hanseulhee <3021062@gmail.com>
2024-12-08 21:21:28 +0900

    [#40] feat: apply web layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import { useMediaQuery } from '@hooks/useMediaQuery';
+import githubIcon from '@public/github.svg';
+import logoIcon from '@public/logo.svg';
 import '@styles/index.css';
 import QueryProvider from 'app/providers/QueryProvider';
 import { LazyMotion, domAnimation } from 'framer-motion';
+import Image from 'next/image';
 import { ReactNode, Suspense } from 'react';
 import KakaoScript from './KakaoScript';
 
@@ -10,24 +16,41 @@ declare global {
   }
 }
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
+  const isWeb = useMediaQuery(730);
+
   return (
-    <html>
+    <html className="overflow-hidden">
       <QueryProvider>
-        <body className="flex flex-row items-center justify-center w-screen h-screen bg-[#e0e2e6]">
-          {/* <AboutService /> */}
-          <div className="relative flex flex-col items-center w-full h-full max-w-[430px] bg-white">
+        <body className="flex items-center justify-center w-screen h-screen bg-[#F6F5F2] gap-[4px] overflow-hidden">
+          {isWeb && (
+            <div className="flex flex-col justify-between items-center h-[60%]">
+              <Image
+                src={logoIcon}
+                alt="logo"
+                width={270}
+                height={320}
+                className="ml-[80px]"
+              />
+              <Image src={githubIcon} alt="github" width={190} height={177} />
+            </div>
+          )}
+
+          <div
+            className={`relative flex flex-col items-center w-full h-full bg-white  ${
+              isWeb
+                ? 'max-w-[390px] max-h-[600px] ml-[100px] rounded-[40px] border-4 border-[#DCEFF9]'
+                : 'max-w-[430px] '
+            }`}
+          >
             <KakaoScript />
             <Suspense fallback={<p>Loading</p>}>
-                 <LazyMotion features={domAnimation}>
-                   {children}
-                 </LazyMotion>
-             </Suspense>
+              <LazyMotion features={domAnimation}>{children}</LazyMotion>
+            </Suspense>
           </div>
         </body>
       </QueryProvider>
