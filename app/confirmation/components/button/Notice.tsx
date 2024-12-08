348e35e14b3086abb24421d9e896798ac3397a86
hanseulhee <3021062@gmail.com>
2024-12-08 20:13:12 +0900

    [#36] feat: create isSigned component

--- app/confirmation/components/button/Notice.tsx
+++ app/confirmation/components/button/Notice.tsx
@@ -1,22 +1,16 @@
-import Image from 'next/image';
 import polygon from '@public/polygon.svg';
+import Image from 'next/image';
 
 export default function Notice({ status }: { status: string }) {
   return (
-    <div className="flex ">
+    <div className="h-full w-full">
       {status !== 'completed' && (
-        <div className="w-[260px] h-[50px]">
-          <div className=" inline-flex p-[8px_24px] justify-center items-center rounded-[8px] bg-[#E0DC51] text-[#000] font-suit text-[17px] font-bold leading-normal">
+        <div className="h-full w-full">
+          <div className="h-full w-full px-5 flex items-center justify-center rounded-[8px] bg-[#E0DC51] text-[#000] font-suit text-[17px] font-bold">
             이름을 눌러 도장을 찍어주세요!
           </div>
-          <div>
-            <Image
-              className="w-[20px] h-[20px] flex-shrink-0"
-              alt="도장찍기"
-              width={20}
-              height={20}
-              src={polygon}
-            />
+          <div className="flex items-center justify-center">
+            <Image alt="도장찍기" src={polygon} />
           </div>
         </div>
       )}
