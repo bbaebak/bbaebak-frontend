1739f559183fa594856850c77a1b4d03b658f560
kimsuns <kimksy27@gmail.com>
2024-12-09 18:10:54 +0900

    [#5] feat: date value edit

--- app/confirmation/components/contents/Date.tsx
+++ app/confirmation/components/contents/Date.tsx
@@ -1,26 +1,10 @@
 import moment from 'moment';
-import Calendar from 'react-calendar';
 
 export default function Date({ value }: { value: string }) {
-  // if (!value) {
-  //   return;
-  // }
-  // const dateObj = new Date(value);
-
-  // ${moment().year()}
-
-  // const formattedDate = dateObj.toLocaleDateString('ko-KR', {
-  //   timeZone: 'Asia/Seoul',
-  //   year: 'numeric',
-  //   month: 'numeric',
-  //   day: 'numeric',
-  // });
-
-  // console.log('vava', value);
-  // const today = new Date();
+  const formattedDate = moment(value).format('YYYY년 MM월 D일');
   return (
     <div className="text-[#51B1E0] font-suit text-[18px] font-medium leading-normal">
-      {value}
+      {formattedDate}
       {/* {  ${moment(value).year()}} */}
     </div>
   );
