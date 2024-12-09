4d4c6241fd70963d1a45980ab69bba46a5e02c67
SJvaca30 <yyvaca30@gmail.com>
2024-12-09 19:45:28 +0900

    [#55] fix: fix Stamp's random number generation using useMemo

--- app/confirmation/components/sign/components/Stamp.tsx
+++ app/confirmation/components/sign/components/Stamp.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import { useMemo } from 'react';
 
 export default function Stamp({ className }: { className: string }) {
-  const randomNum = Math.floor(Math.random() * 13) + 1;
+  const randomNum = useMemo(() => Math.floor(Math.random() * 13) + 1, []);
+
   return (
-    <div className={`${className} w-[58px] h-[65.296px] shrink-0 `}>
+    <div className={`${className} w-[58px] h-[65.296px] shrink-0`}>
       <Image
         src={`/stamp/${randomNum}.svg`}
         alt="서명 스탬프"
